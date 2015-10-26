c608a8ec67cbd677e56a4a6bdebf28f3841bd7fd
pratap bunga <pratap234337@gmail.com>
2015-10-26 09:36:00 -0700

    1st commit, set up photogrid app

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+module.exports = function(express, app){
+	var router = express.Router();
+
+	router.get('/', function(req, res, next){
+		res.render('index', {});
+	})
+
+	app.use('/', router);
+}
+
